e49d859a06ff457578cdd7e32ecdb101f5b0f3fc
Harsh Dave <harsh.dave@odido.nl>
2025-01-21 06:54:05 +0530

    Prod Ready

---
+++ build/precache-manifest.bc64fce6cd2a83d8be357f1c4f34866c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387e3b60392c3d82690b7c67ad766651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1812bffc0ccaed6b557",
+    "url": "/static/css/main.a75ca5a7.chunk.css"
+  },
+  {
+    "revision": "01fd0823087a2ddb3d0f",
+    "url": "/static/js/2.aa42c61c.chunk.js"
+  },
+  {
+    "revision": "c1812bffc0ccaed6b557",
+    "url": "/static/js/main.35a887d8.chunk.js"
+  },
+  {
+    "revision": "2683c2bc45b5d15d3b2d",
+    "url": "/static/js/runtime-main.9f46c223.js"
+  },
+  {
+    "revision": "5f86af978913654b7f6279eb19782687",
+    "url": "/static/media/like.5f86af97.png"
+  },
+  {
+    "revision": "eb77bfb55efc0021e68a6186ce1cadc1",
+    "url": "/static/media/p1.eb77bfb5.jpg"
+  },
+  {
+    "revision": "d13486a103ab741205326db427566332",
+    "url": "/static/media/p2.d13486a1.jpg"
+  },
+  {
+    "revision": "2d13453bfed9b8622b5e2abd7399d7c9",
+    "url": "/static/media/p3.2d13453b.jpg"
+  },
+  {
+    "revision": "b33b9181020763573debbb34c3920778",
+    "url": "/static/media/p4.b33b9181.jpg"
+  },
+  {
+    "revision": "b4ea08bb3b40e600dbe457608289ccb3",
+    "url": "/static/media/p5.b4ea08bb.jpg"
+  },
+  {
+    "revision": "57b8821639388c70be74a4d8a3a54ff7",
+    "url": "/static/media/p6.57b88216.jpg"
+  },
+  {
+    "revision": "4164b7fa32d18a065900748edc6c4b09",
+    "url": "/static/media/shape2.4164b7fa.png"
+  }
+]);
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import { navItems } from "lib/data/navItems";
+
+const Navbar = () => {
+  return (
+    <header className="header-transparent">
+      <nav className="navbar navbar-expand-lg text-center">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+          PixelPulse Media
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon">
+              <i className="fi-xwluxl-three-bars-wide"></i>
+            </span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {navItems.map((item) => {
+              return (
+                <ul className="navbar-nav ml-auto" key={item.id}>
+                  <li className="nav-item">
+                    <Link to={item.link} className="nav-link">
+                      {item.name}
+                    </Link>
+                  </li>
+                </ul>
+              );
+            })}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
